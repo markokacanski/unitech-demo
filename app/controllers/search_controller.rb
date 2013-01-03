67464201c15e524f51d1d39b8f111bdd0a3b0931
levnajic <milan.levnajic@gmail.com>
2013-01-03 22:50:35 +0100

    porpavljena search strana ubacen brand i pod brend meni

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,33 @@
 class SearchController < ApplicationController
   def index
+  	if params[:brands]
+      brands = params[:brands].split("/")
+
+      brands.each_index do |i|
+        brands[i] = brands[i].tr("_", " ")
+      end
+
+      if brands[1]
+        @items = UnitechItem.where("
+          manufacturer = '#{brands[0]}'
+          AND manufacturer_pb = '#{brands[1]}'
+        ")
+      elsif brands[0]
+        @items = UnitechItem.where("
+          manufacturer = '#{brands[0]}'
+        ")
+      else
+        @items = UnitechItem.all
+      end
+    else
+      @items = UnitechItem.all
+    end
+
+    manufacturers = UnitechItem.get_manufacturers
+    @brands = Hash.new
+    manufacturers.each do |manufacturer|
+      @brands["#{manufacturer}"] = UnitechItem.get_podbrends(manufacturer)
+    end
     @items = UnitechItem.search(params[:search])
   end
 end
