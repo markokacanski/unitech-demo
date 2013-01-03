20f4ff31df01063f85b933192d5d045ee701ea2b
Marko Kacanski <marko.kacanski@gmail.com>
2013-01-03 18:56:08 +0100

    added brand url filtering

--- app/controllers/brand_controller.rb
+++ app/controllers/brand_controller.rb
@@ -1,4 +1,21 @@
 class BrandController < ApplicationController
   def show
+    if params[:brands]
+      brands = params[:brands].split("/")
+
+      if brands[1]
+        @items = UnitechItem.where("
+          manufacturer = '#{brands[0]}'
+          AND manufacturer_pb = '#{brands[1]}'
+        ")
+      elsif brands[0]
+        @items = UnitechItem.where("
+          manufacturer = '#{brands[0]}'
+        ")
+      else
+        @items = UnitechItem.all
+      end
+
+    end
   end
 end
